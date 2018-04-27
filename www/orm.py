0ba1d6a56d9275e255602e3f67abc4b4ee51a8bd
horizon <m17759502859@163.com>
2018-04-27 23:32:26 +0800

    Day 11

--- www/orm.py
+++ www/orm.py
@@ -209,9 +209,9 @@ class Model(dict,metaclass=ModelMetaclass):
         return rs[0]['_num_']
 
     @classmethod
-    async def find(cls,pk):
+    async def find(cls, pk):
         'find object by primary key.'
-        rs = await select('%s where `%s` = ?' % (cls.__select__,cls.primarykey),[pk],1)
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
         if len(rs) == 0:
             return None
         return cls(**rs[0])
